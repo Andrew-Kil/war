a4d6c5f6ce1d67d555b90411a83eb6b8dafe1b58
Andrew KIl <kil.andrew@gmail.com>
2020-06-01 22:48:04 -0400

    add functions to deck utils

--- src/utils/deck.js
+++ src/utils/deck.js
@@ -25,11 +25,29 @@ export const createDeck = () =>
     []
   );
 
-export const shuffle = (deck) => {
+export const shuffleDeck = (deck) => {
   for (let i = deck.length; i; i--) {
     let j = Math.floor(Math.random() * i);
     [deck[i - 1], deck[j]] = [deck[j], deck[i - 1]];
   }
+  return deck;
 };
 
-export const drawCard = (deck) => deck.shift();
+export const splitDeck = (deck) => [
+  [...deck.slice(0, 26)],
+  [...deck.slice(26, 52)],
+];
+
+export const drawCard = (deck) => deck.slice(1);
+
+export const compareCards = (playerOneCard, playerTwoCard) => {
+  if (RANKS.indexOf(playerOneCard.rank) > RANKS.indexOf(playerTwoCard.rank)) {
+    return "PLAYER_ONE";
+  } else if (
+    RANKS.indexOf(playerOneCard.rank) < RANKS.indexOf(playerTwoCard.rank)
+  ) {
+    return "PLAYER_TWO";
+  } else {
+    return "WAR";
+  }
+};
