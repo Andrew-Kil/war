d7b8f9270348922a7424bb4307c108d368c7b048
Andrew KIl <kil.andrew@gmail.com>
2020-06-01 22:48:42 -0400

    initial Game component - work in progress

---
+++ src/components/Game/index.js
@@ -0,0 +1,178 @@
+import React, { useState } from "react";
+
+import {
+  createDeck,
+  shuffleDeck,
+  splitDeck,
+  drawCard,
+  compareCards,
+} from "../../utils/deck";
+
+const Game = () => {
+  const [gameStatus, setGameStatus] = useState({
+    cardsDealt: false,
+    isGameInProgress: false,
+  });
+  const [playerOneCards, setPlayerOneCards] = useState({
+    deck: [],
+    discardPile: [],
+    currentCard: {},
+  });
+  const [playerTwoCards, setPlayerTwoCards] = useState({
+    deck: [],
+    discardPile: [],
+    currentCard: {},
+  });
+
+  let newDeck = null;
+
+  const dealDeck = () => {
+    newDeck = splitDeck(shuffleDeck(createDeck()));
+    console.log(newDeck);
+    setGameStatus({ ...gameStatus, cardsDealt: true });
+    setPlayerOneCards({
+      ...playerOneCards,
+      deck: newDeck[0],
+      currentCard: newDeck[0][0],
+    });
+    setPlayerTwoCards({
+      ...playerTwoCards,
+      deck: newDeck[1],
+      currentCard: newDeck[1][0],
+    });
+  };
+
+  const handleDrawCard = (playerOneCards, playerTwoCards) => {
+    setPlayerOneCards({
+      ...playerOneCards,
+      deck: drawCard(playerOneCards.deck),
+      currentCard: playerOneCards.deck.shift(),
+    });
+    setPlayerTwoCards({
+      ...playerTwoCards,
+      deck: drawCard(playerTwoCards.deck),
+      currentCard: playerTwoCards.deck.shift(),
+    });
+    setGameStatus({ ...gameStatus, isGameInProgress: true });
+  };
+
+  const checkDeckAndDiscardPile = (playerCards, setPlayerCards) => {
+    if (isEmpty(playerCards.deck)) {
+      if (isEmpty(playerCards.discardPile)) {
+        // game over
+        setGameStatus({ ...gameStatus, isGameInProgress: false });
+        console.log("GAME OVER");
+      } else {
+        // shuffle discard and move to deck
+        setPlayerCards({
+          ...playerCards,
+          deck: shuffleDeck(playerCards.discardPile),
+          discardPile: null,
+        });
+      }
+    }
+  };
+
+  const handleTurn = () => {
+    handleDrawCard(playerOneCards, playerTwoCards);
+
+    console.log("playerOne card: ", playerOneCards.currentCard.rank);
+    console.log("playerTwo card: ", playerTwoCards.currentCard.rank);
+
+    if (
+      compareCards(playerOneCards.currentCard, playerTwoCards.currentCard) ===
+      "PLAYER_ONE"
+    ) {
+      console.log("PLAYER ONE WINS ROUND");
+      // remove current card from playerTwo
+      // add both cards to playerTwo discard pile
+      setPlayerTwoCards({
+        ...playerTwoCards,
+        deck: [...playerTwoCards.deck.slice(1)],
+        currentCard: [...playerTwoCards.deck.slice(1)][0],
+      });
+      setPlayerOneCards({
+        ...playerOneCards,
+        deck: [...playerOneCards.deck.slice(1)],
+        currentCard: [...playerOneCards.deck.slice(1)][0],
+        discardPile: [
+          ...playerOneCards.discardPile,
+          playerOneCards.currentCard,
+          playerTwoCards.currentCard,
+        ],
+      });
+    } else if (
+      compareCards(playerOneCards.currentCard, playerTwoCards.currentCard) ===
+      "PLAYER_TWO"
+    ) {
+      console.log("PLAYER TWO WINS ROUND");
+      // remove current card from playerTwo
+      // add both cards to playerTwo discard pile
+      setPlayerOneCards({
+        ...playerOneCards,
+        deck: [...playerOneCards.deck.slice(1)],
+      });
+      setPlayerTwoCards({
+        ...playerTwoCards,
+        deck: [...playerTwoCards.deck.slice(1)],
+        discardPile: [
+          ...playerTwoCards.discardPile,
+          playerTwoCards.currentCard,
+          playerOneCards.currentCard,
+        ],
+      });
+    } else {
+      // WAR
+    }
+
+    checkDeckAndDiscardPile(playerOneCards, setPlayerOneCards);
+    checkDeckAndDiscardPile(playerTwoCards, setPlayerTwoCards);
+  };
+
+  const isEmpty = (cards) => (cards.length === 0 ? true : false);
+
+  console.log(playerOneCards);
+  console.log(playerTwoCards);
+
+  return (
+    <>
+      <h1>Game</h1>
+
+      {gameStatus.cardsDealt ? (
+        <button onClick={() => handleTurn()}>Draw</button>
+      ) : (
+        <button onClick={() => dealDeck()}>Deal</button>
+      )}
+
+      {!!(playerOneCards.deck.length + playerOneCards.discardPile.length) && (
+        <>
+          <h3>Player One</h3>
+          <div>
+            Current Card: {playerOneCards.currentCard.rank} of{" "}
+            {playerOneCards.currentCard.suit}
+          </div>
+          <div>
+            Number of Cards:{" "}
+            {playerOneCards.deck.length + playerOneCards.discardPile.length}
+          </div>
+        </>
+      )}
+
+      {!!(playerTwoCards.deck.length + playerTwoCards.discardPile.length) && (
+        <>
+          <h3>Player Two</h3>
+          <div>
+            Current Card: {playerTwoCards.currentCard.rank} of{" "}
+            {playerTwoCards.currentCard.suit}
+          </div>
+          <div>
+            Number of Cards:{" "}
+            {playerTwoCards.deck.length + playerTwoCards.discardPile.length}
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Game;
